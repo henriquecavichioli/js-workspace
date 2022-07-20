be4ecf4a8abc1a3e39c20d228f40067ebe2ec264
Henrique Cavichioli <henriqueemann@gmail.com>
2022-07-19 23:41:59 -0300

    feat: regex adicionado

--- introducao-javascript/js/filtro.js
+++ introducao-javascript/js/filtro.js
@@ -1,20 +1,29 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
-campoFiltro.addEventListener("input", function() {
 
-var pacientes = document.querySelectorAll(".paciente");
-for (var i = 0; i < pacientes.length; i++ ) {
+campoFiltro.addEventListener("input", filtraPorNome);
 
-    var tdNomePaciente = pacientes[i].querySelector(".info-nome");
-    var nomePaciente = tdNomePaciente.textContent;
-    var nomeFiltrado = this.value;
+function filtraPorNome() {
 
-    console.log(`nomeFiltrado = ${nomeFiltrado}`);
-
-    if (nomePaciente != nomeFiltrado) {
-        pacientes[i].classList.add("invisivel");
+    var pacientes = document.querySelectorAll(".paciente");
+    if (campoFiltro.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++ ) {
+    
+            var tdNomePaciente = pacientes[i].querySelector(".info-nome");
+            var nomePaciente = tdNomePaciente.textContent;
+            var nomeFiltrado = campoFiltro.value;
+            var expressao = new RegExp(nomeFiltrado, "i");
+    
+            if (!expressao.test(nomePaciente)) {
+                pacientes[i].classList.add("invisivel");
+            } else {
+                pacientes[i].classList.remove("invisivel");
+            }
+        }
     } else {
-        pacientes[i].classList.remove("invisivel");
+    
+        for (var i = 0; i < pacientes.length; i++ ) {
+            pacientes[i].classList.remove("invisivel");
+    
+        }
     }
 }
-
-})
